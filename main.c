aeef36d33158b31572fd632178986e5b2f3fcace
Xvezda <xvezda@naver.com>
2020-03-07 04:20:34 +0900

    Add read from file feature

--- main.c
+++ main.c
@@ -6,32 +6,77 @@
  * https://opensource.org/licenses/MIT.
  */
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "toyson.h"
 
 
+static void read_from_file(char **ref, char *fname);
+
+
+static int is_file;
+
 int main(int argc, char **argv)
 {
-    char *json = \
-        "{\n"
-        "  \"foo\": \"bar\","
-        "  \"baz\": {"
-            "\"hello\": \"world\","
-            " \"qwer\": 12345"
-            "},\n"
-        "\t\"asdf\": [123, 456, \"789\"],\n"
-        "\"zxcv\": [[{}, {"
-                "\"abc\": true, \"def\": false"
-            "}, \"uiop\", null], \"hjkl\", true ]\n"
-        "}";
+    int count = 0;
+    char *json = NULL;
 
-    toyson_t toyson;
-    toyson_init(&toyson);
-    toyson_parser(&toyson, json);
+    if (argc < 2) {
+        count = 1;
+        json = \
+            "{\n"
+            "  \"foo\": \"bar\","
+            "  \"baz\": {"
+                "\"hello\": \"world\","
+                " \"qwer\": 12345"
+                "},\n"
+            "\t\"asdf\": [123, 456, \"789\"],\n"
+            "\"zxcv\": [[{}, {"
+                    "\"abc\": true, \"def\": false"
+                "}, \"uiop\", null], \"hjkl\", true ]\n"
+            "}";
+    } else {
+        is_file = 1;
+        count = argc - 1;
+    }
 
-    printf("json: %s\n", json);
-    toyson_print(&toyson);
+    for (int i = 0; i < count; ++i) {
+        if (is_file) read_from_file(&json, argv[i+1]);
 
-    toyson_del(&toyson);
+        toyson_t toyson;
 
-    return 0;
+        toyson_init(&toyson);
+        toyson_parser(&toyson, json);
+
+        printf("json: %s\n", json);
+        toyson_print(&toyson);
+
+        toyson_del(&toyson);
+
+        if (is_file) free(json);
+    }
+
+    return EXIT_SUCCESS;
 }
+
+
+static void read_from_file(char **ref, char *fname)
+{
+    FILE *fp;
+    if ((fp = fopen(fname, "r"))) {
+        fseek(fp, 0, SEEK_END);
+        size_t fsize = ftell(fp);
+        fseek(fp, 0, SEEK_SET);
+
+        *ref = malloc(fsize + 1);
+
+        fread(*ref, 1, fsize, fp);
+        fclose(fp);
+
+        (*ref)[fsize] = '\0';
+    } else {
+        is_file = 0;
+        *ref = "{}";
+    }
+}
+
